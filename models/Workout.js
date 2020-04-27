7681e4d6bd743025078e9e92525512360ec91be4
annalisamacaluso <55858533+annalisamacaluso@users.noreply.github.com>
2020-04-27 13:34:14 -0700

    committing files

---
+++ models/Workout.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+      type: Date,
+      default: Date.now()
+  },
+  exercises: [
+      {
+        name: String,
+        type: {
+          type: String
+        },
+        weight: Number,
+        sets: Number,
+        reps: Number,
+        duration: {
+            type: Number,
+            required: "Enter a duration"
+        },
+        distance: Number
+      }
+  ]
+}, { toJSON: { virtuals: true } });
+
+WorkoutSchema.virtual("totalDuration").get(function(){
+  let totalD = this.exercises.reduce((sum, currentObject) => sum + currentObject.duration, 0);
+  return totalD;
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
